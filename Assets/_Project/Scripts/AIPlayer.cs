8946f4f053ab97b9875fc83cc5c34ec316dbda5b
bahaa <dev.bahaasrour@gmail.com>
2024-01-18 00:30:34 +0200

    Update AIPlayer.cs

--- Assets/_Project/Scripts/AIPlayer.cs
+++ Assets/_Project/Scripts/AIPlayer.cs
@@ -5,26 +5,13 @@ using UnityEngine;
 
 public class AIPlayer : Player
 {
-    private void Awake()
-    {
-        OnPlayerRecieveCards.addListener(SortCards);
-    }
-
-    private void SortCards()
-    {
-        for (int i = 0; i < myCards.Count; i++)
-        {
-            
-        }
-    }
-
     public override void StartTurn()
     {
         StartCoroutine(playCard());
     }
     IEnumerator playCard()
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(.5f);
         SelectCardToPlay(myCards[0]);
     }
 }
